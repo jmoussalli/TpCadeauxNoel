67c5f8d145780773b6842de38418e096a39f6755
jmoussalli <jerome.moussalli@gmail.com>
2024-01-30 13:49:30 +0100

    First commit

---
+++ src/main/java/fr/moussalli/tpcadeauxnoel/TpCadeauxNoelApplication.java
@@ -0,0 +1,13 @@
+package fr.moussalli.tpcadeauxnoel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpCadeauxNoelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpCadeauxNoelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/moussalli/tpcadeauxnoel/TpCadeauxNoelApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.moussalli.tpcadeauxnoel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpCadeauxNoelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
